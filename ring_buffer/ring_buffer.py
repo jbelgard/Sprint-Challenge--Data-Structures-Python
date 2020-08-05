2535e9ba205c8334f52c31f01c6f926bda54071f
Jason Belgard <belgard.jason@gmail.com>
2020-08-04 21:59:36 -0500

    First go with Ring Buffer

--- ring_buffer/ring_buffer.py
+++ ring_buffer/ring_buffer.py
@@ -1,9 +1,16 @@
 class RingBuffer:
     def __init__(self, capacity):
-        pass
+        self.queue = []
+        self.capacity = capacity
+        self.indexOfOldestItem = 0
 
     def append(self, item):
-        pass
+        if len(self.queue) < self.capacity:
+            self.queue.append(item)
+        else:
+            self.queue[self.indexOfOldestItem] = item
+            shouldReset = True if self.indexOfOldestItem == self.capacity else False
+            self.indexOfOldestItem = 0 if shouldReset else self.indexOfOldestItem + 1
 
     def get(self):
-        pass
+        return self.queue
